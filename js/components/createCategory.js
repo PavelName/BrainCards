7d7d320df7b2f6d0bde4a50edf72a66208dec45e
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2023-04-26 11:19:11 +0800

    function helper

--- js/components/createCategory.js
+++ js/components/createCategory.js
@@ -1,5 +1,5 @@
 import { createElement } from "../helper/createElement.js";
-
+import { declOfNum } from "../helper/declOfNum.js";
 
 export const createCategories = (app) => {
 
@@ -20,11 +20,37 @@ export const createCategories = (app) => {
     const createCategoryCard = (data) => {
         const item = createElement('li', {
             className: 'category__item',
-            textContent: data.title, // в дальнейшем убрать
+            
         });
         item.dataset.id = data.id;
 
-      //дописать карточку  
+        const btnCard = createElement('button', {
+            className: 'category__card',
+        });
+
+        const titleText = createElement('span', {
+            className: 'category__title',
+            textContent: data.title,
+        });
+
+        const pairsText = createElement('span', {
+            className: 'category__pairs',
+            textContent: declOfNum(data.length, ['пара', 'пары', 'пар']),
+        });
+
+        btnCard.append(titleText, pairsText);
+
+        const btnEdit = createElement('button', {
+            className: 'category__btn category__edit',
+            ariaLable: 'редактировать',
+        });
+
+        const btnDel = createElement('button', {
+            className: 'category__btn category__del',
+            ariaLable: 'удалить',
+        });
+
+       item.append(btnCard, btnDel, btnEdit);
 
         return item;
     }
