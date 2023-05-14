e5e03f4f5e1d0e1c36b9f1e7792285af2977f16a
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2023-05-14 17:14:14 +0800

    render

--- js/components/createPairs.js
+++ js/components/createPairs.js
@@ -8,19 +8,38 @@ export const createPairs = (app) => {
     const container = createElement('div', {
         className: 'container card__container',
     });
-    
+
+    const buttonReturn = createElement('button', {
+        className: 'card__return',
+        ariaLabel: 'Возврат к категориям',
+    });
+
+    const buttonCard = createElement('button', {
+        className: 'card__item',
+    });
+
+    const front = createElement('span', {
+        className: 'card__front',
+        textContent: 'one',
+    });
+
+    const back = createElement('span', {
+        className: 'card__back',
+        textContent: 'two',
+    });
+
+    buttonCard.append(front, back);
+    container.append(buttonReturn, buttonCard);
+    pairs.append(container);
+
+    const mount = data => {
+        app.append(pairs);
+    };
+
+    const unmount = () => {
+        pairs.remove();
+    };
+    return {buttonReturn, mount, unmount};
 
 };
 
-/* 
-<section class="card section-offset">
-      <div class="container card__container">
-        <button class="card__return" aria-label="Возврат к категориям"></button>
-        <button class="card__item">
-          <span class="card__front">улыбка</span>
-          <span class="card__back">smile</span>
-        </button>
-      </div>
-    </section>
-
-*/
